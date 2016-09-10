0463ba1d156d84c94772b4216146458d6f279747
Zac <zknoebel@gmail.com>
2016-09-09 18:33:30 -1000

    Initial commit 	new file:   .classpath 	new file:   .gitignore 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   src/CompareNumbers.java 	new file:   src/Contact.java 	new file:   src/ContactComparator.java 	new file:   src/List211.java 	new file:   src/MyArrayList.java

---
+++ src/List211.java
@@ -0,0 +1,10 @@
+
+public interface List211<E> {
+
+  boolean add(E e);
+  void add(int index, E element);
+  E get(int index);
+  E remove(int index);
+  E set(int index, E element);
+  int size();
+}
---
+++ src/MyArrayList.java
@@ -0,0 +1,59 @@
+import java.util.Comparator;
+
+public class MyArrayList<E> implements List211<E> {
+
+  @Override
+  public boolean add(E e) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+
+  @Override
+  public void add(int index, E element) {
+    // TODO Auto-generated method stub
+
+  }
+
+
+  @Override
+  public E get(int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+  @Override
+  public E remove(int index) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+  @Override
+  public E set(int index, E element) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+
+  @Override
+  public int size() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+  
+  private E[] data;
+  private int size;
+  
+  public void insertionSort(Comparator<? super E> compare){
+    
+  }
+  public void bubbleSort(Comparator<? super E> compare){
+    
+  }
+  public void selectionSort(Comparator<? super E> compare){
+    
+  }
+
+}
